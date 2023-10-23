8a16cdb8339a694e60ac1471fa93992552bfcb07
JoaoVitor <joao12345vitorps2@gmail.com>
2023-10-22 23:22:06 -0300

    fix

--- ConnectFitMobile/lib/Pages/TrainingPage/training_list.dart
+++ ConnectFitMobile/lib/Pages/TrainingPage/training_list.dart
@@ -26,193 +26,189 @@ class _TrainingListState extends State<TrainingList> {
         actions: [],
       ),
       body: SingleChildScrollView(
-        child: Flexible(
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(
-                  right: 30,
-                  left: 30,
-                  top: 15,
-                  bottom: 15,
-                ),
-                child: Row(
-                  children: [
-                    const Expanded(
-                      child: SizedBox(
-                        height: 25,
-                        child: TextField(
-                          decoration: InputDecoration(
-                            contentPadding: EdgeInsets.symmetric(
-                                vertical: 10, horizontal: 20),
-                            filled: true,
-                            fillColor: Colors.brancoBege,
-                            labelText: 'Buscar',
-                            border: OutlineInputBorder(
-                              borderSide: BorderSide.none,
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(5),
-                              ),
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(
+                right: 30,
+                left: 30,
+                top: 15,
+                bottom: 15,
+              ),
+              child: Row(
+                children: [
+                  const Expanded(
+                    child: SizedBox(
+                      height: 25,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.symmetric(
+                              vertical: 10, horizontal: 20),
+                          filled: true,
+                          fillColor: Colors.brancoBege,
+                          labelText: 'Buscar',
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(5),
                             ),
                           ),
                         ),
                       ),
                     ),
-                    IconButton(
-                      onPressed: () {
-                        showModalBottomSheet<void>(
-                          backgroundColor: Colors.transparent,
-                          context: context,
-                          builder: (BuildContext context) {
-                            return Container(
-                              decoration: const BoxDecoration(
-                                color: Colors.begeclaro,
-                                borderRadius: BorderRadius.only(
-                                    topRight: Radius.circular(20),
-                                    topLeft: Radius.circular(20)),
-                              ),
-                              height: MediaQuery.of(context).size.height * 0.5,
-                              child: Center(
-                                child: Column(
-                                  children: <Widget>[
-                                    Align(
-                                      alignment: Alignment.centerRight,
-                                      child: IconButton(
-                                          icon: const Icon(Icons.close),
-                                          onPressed: () =>
-                                              Navigator.pop(context)),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            );
-                          },
-                        );
-                      },
-                      icon: const Icon(Icons.filter_alt),
-                      color: Colors.brancoBege,
-                      iconSize: 30,
-                    )
-                  ],
-                ),
-              ),
-              Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 30, right: 30),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        CustomText(
-                          text: _selectedStatus,
-                        ),
-                        PopupMenuButton<String>(
-                          icon: const Icon(Icons.filter_list),
-                          color: Colors.white,
-                          onSelected: _applyFilter,
-                          itemBuilder: (BuildContext context) {
-                            return [
-                              const PopupMenuItem(
-                                value: 'Ativo',
-                                child: Text('Ativo'),
-                              ),
-                              const PopupMenuItem(
-                                value: 'Concluido',
-                                child: Text('Concluído'),
-                              ),
-                              const PopupMenuItem(
-                                value: 'Todas',
-                                child: Text('Todas'),
-                              ),
-                            ];
-                          },
-                        ),
-                      ],
-                    ),
                   ),
-                  ListView.builder(
-                    shrinkWrap: true,
-                    physics: const NeverScrollableScrollPhysics(),
-                    itemCount: 3,
-                    itemBuilder: (context, index) {
-                      if (_selectedStatus != "Todas" &&
-                          _selectedStatus != fichas[index]['status']) {
-                        return const SizedBox
-                            .shrink(); // Retorna um widget vazio
-                      }
-                      return Padding(
-                        padding: const EdgeInsets.only(top: 15),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: Colors.pretoPag,
-                            borderRadius: const BorderRadius.all(
-                              Radius.circular(10),
+                  IconButton(
+                    onPressed: () {
+                      showModalBottomSheet<void>(
+                        backgroundColor: Colors.transparent,
+                        context: context,
+                        builder: (BuildContext context) {
+                          return Container(
+                            decoration: const BoxDecoration(
+                              color: Colors.begeclaro,
+                              borderRadius: BorderRadius.only(
+                                  topRight: Radius.circular(20),
+                                  topLeft: Radius.circular(20)),
                             ),
-                            border: Border.all(
-                              color: fichas[index]['status'] == 'Ativo'
-                                  ? Colors.green
-                                  : Colors.red,
-                              width: 2,
-                            ),
-                          ),
-                          margin: const EdgeInsetsDirectional.only(
-                            start: 30,
-                            end: 30,
-                          ),
-                          child: ExpansionTile(
-                            collapsedIconColor: Colors.white,
-                            iconColor: Colors.white,
-                            title: CustomText(
-                              text: "${fichas[index]['nome']}",
-                              isBold: true,
-                            ),
-                            subtitle: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                CustomText(
-                                    text:
-                                        "Início: ${fichas[index]['dataInicio']}"),
-                                fichas[index]['dataFim'] != null
-                                    ? CustomText(
-                                        text:
-                                            "Fim: ${fichas[index]['dataFim']}")
-                                    : const SizedBox.shrink()
-                              ],
-                            ),
-                            children: <Widget>[
-                              ListView.builder(
-                                shrinkWrap: true,
-                                physics: const NeverScrollableScrollPhysics(),
-                                itemCount: 5,
-                                itemBuilder: (context, index) {
-                                  return Padding(
-                                    padding: const EdgeInsets.only(
-                                        right: 40, left: 40),
-                                    child: Card(
-                                      color: Colors.deepOrange,
-                                      child: ListTile(
-                                        onTap: () {
-                                          Navigator.pushNamed(
-                                              context, '/formDetails',
-                                              arguments: (index + 1));
-                                        },
-                                        textColor: Colors.white,
-                                        title: Text(textos[index]),
-                                      ),
-                                    ),
-                                  );
-                                },
+                            height: MediaQuery.of(context).size.height * 0.5,
+                            child: Center(
+                              child: Column(
+                                children: <Widget>[
+                                  Align(
+                                    alignment: Alignment.centerRight,
+                                    child: IconButton(
+                                        icon: const Icon(Icons.close),
+                                        onPressed: () =>
+                                            Navigator.pop(context)),
+                                  ),
+                                ],
                               ),
-                            ],
-                          ),
-                        ),
+                            ),
+                          );
+                        },
                       );
                     },
-                  ),
+                    icon: const Icon(Icons.filter_alt),
+                    color: Colors.brancoBege,
+                    iconSize: 30,
+                  )
                 ],
               ),
-            ],
-          ),
+            ),
+            Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 30, right: 30),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      CustomText(
+                        text: _selectedStatus,
+                      ),
+                      PopupMenuButton<String>(
+                        icon: const Icon(Icons.filter_list),
+                        color: Colors.white,
+                        onSelected: _applyFilter,
+                        itemBuilder: (BuildContext context) {
+                          return [
+                            const PopupMenuItem(
+                              value: 'Ativo',
+                              child: Text('Ativo'),
+                            ),
+                            const PopupMenuItem(
+                              value: 'Concluido',
+                              child: Text('Concluído'),
+                            ),
+                            const PopupMenuItem(
+                              value: 'Todas',
+                              child: Text('Todas'),
+                            ),
+                          ];
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+                ListView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: 3,
+                  itemBuilder: (context, index) {
+                    if (_selectedStatus != "Todas" &&
+                        _selectedStatus != fichas[index]['status']) {
+                      return const SizedBox.shrink(); // Retorna um widget vazio
+                    }
+                    return Padding(
+                      padding: const EdgeInsets.only(top: 15),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.pretoPag,
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(10),
+                          ),
+                          border: Border.all(
+                            color: fichas[index]['status'] == 'Ativo'
+                                ? Colors.green
+                                : Colors.red,
+                            width: 2,
+                          ),
+                        ),
+                        margin: const EdgeInsetsDirectional.only(
+                          start: 30,
+                          end: 30,
+                        ),
+                        child: ExpansionTile(
+                          collapsedIconColor: Colors.white,
+                          iconColor: Colors.white,
+                          title: CustomText(
+                            text: "${fichas[index]['nome']}",
+                            isBold: true,
+                          ),
+                          subtitle: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              CustomText(
+                                  text:
+                                      "Início: ${fichas[index]['dataInicio']}"),
+                              fichas[index]['dataFim'] != null
+                                  ? CustomText(
+                                      text: "Fim: ${fichas[index]['dataFim']}")
+                                  : const SizedBox.shrink()
+                            ],
+                          ),
+                          children: <Widget>[
+                            ListView.builder(
+                              shrinkWrap: true,
+                              physics: const NeverScrollableScrollPhysics(),
+                              itemCount: 5,
+                              itemBuilder: (context, index) {
+                                return Padding(
+                                  padding: const EdgeInsets.only(
+                                      right: 40, left: 40),
+                                  child: Card(
+                                    color: Colors.deepOrange,
+                                    child: ListTile(
+                                      onTap: () {
+                                        Navigator.pushNamed(
+                                            context, '/formDetails',
+                                            arguments: (index + 1));
+                                      },
+                                      textColor: Colors.white,
+                                      title: Text(textos[index]),
+                                    ),
+                                  ),
+                                );
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
