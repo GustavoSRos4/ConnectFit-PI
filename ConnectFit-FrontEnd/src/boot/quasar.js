5e53989cf8f87a114b1ec89e4054aa1200321e38
Henrique Oliveira Melo <104600158+MrHenrique@users.noreply.github.com>
2023-10-29 16:37:32 -0300

    Revert "Merge branch 'webBranch'"
    This reverts commit 400c56920ff8e41c6807d30fb12663d0aa62511d, reversing
    changes made to 57b49109bd281c387fdf4f0f5d5f7a91981fba63.

--- ConnectFit-FrontEnd/src/boot/quasar.js
+++
@@ -1,7 +0,0 @@
-// src/boot/quasar.js
-import { boot } from 'quasar/wrappers';
-import httpProxyMiddleware from './httpProxyMiddleware';
-
-export default boot(({ app }) => {
-  httpProxyMiddleware(app); // Chame a função de configuração do middleware
-});
