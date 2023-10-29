6e30400be85bbdf63108240a219aaa514eac7c48
Henrique Oliveira Melo <104600158+MrHenrique@users.noreply.github.com>
2023-10-29 16:37:13 -0300

    Revert "Revert "Merge branch 'Branch-IntegracaoSys'""
    This reverts commit a5143f6b1035f2f73136d93f0641e8121d5d6537.

---
+++ ConnectFitMobile/lib/Pages/HomePage/home.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:projeto/Pages/ContratoPage/page_contrato.dart';
+import 'package:projeto/Pages/HomePage/home_page.dart';
+import 'package:projeto/Pages/PerfilPage/perfil.dart';
+import 'package:projeto/Pages/PersonalPage/personal_page.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int currentIndex = 0;
+
+  final _pageOptions = [
+    const HomePage(),
+    const PersonalPage(),
+    const PageContrato(),
+    const PerfilPage(),
+  ];
+  void _onIndexChanged(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pageOptions[currentIndex],
+      bottomNavigationBar: BottomNavBar(
+        onIndexChanged: _onIndexChanged,
+      ),
+    );
+  }
+}
+
+class BottomNavBar extends StatefulWidget {
+  final ValueChanged<int> onIndexChanged;
+
+  const BottomNavBar({Key? key, required this.onIndexChanged})
+      : super(key: key);
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  var _currentIndex = 0;
+
+  void setIndex(int index) {
+    setState(() {
+      _currentIndex = index;
+      widget.onIndexChanged(index);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      selectedItemColor: Colors.deepOrange,
+      showUnselectedLabels: false,
+      useLegacyColorScheme: false,
+      currentIndex: _currentIndex,
+      onTap: setIndex,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: "Início",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.search),
+          label: "Personal",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.wallet_travel),
+          label: "Contrato",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.account_box),
+          label: "Perfil",
+        )
+      ],
+    );
+  }
+}
