30ebeff180bcd43381613f632a54aacf6646312f
Gustavo Silvério Rosa <gustavorosasilverio@gmail.com>
2023-10-29 17:02:13 -0300

    teste

--- ConnectFit-FrontEnd/src/pages/ErrorNotFound.vue
+++ ConnectFit-FrontEnd/src/pages/ErrorNotFound.vue
@@ -6,7 +6,7 @@
       </div>
 
       <div class="text-h2" style="opacity:.4">
-        Oops. Nothing here...
+        Oops. Nothing heree...
       </div>
 
       <q-btn class="q-mt-xl" color="white" text-color="blue" unelevated to="/" label="Go Home" no-caps />
